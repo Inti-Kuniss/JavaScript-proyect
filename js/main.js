b40a5559e6c66a3ae4fe70894a2cb80866f5520f
Inti-Kuniss <126829303+Inti-Kuniss@users.noreply.github.com>
2023-05-16 20:16:26 -0300

    v.2.0

--- js/main.js
+++ js/main.js
@@ -1,77 +1,40 @@
-// Función para calcular el costo del seguro de automóvil
-function calcularSeguroAuto(modelo, edad, lugar) {
-  let costo = 0;
+function consultarSA(marca, modelo, version, ano){
+  const fs = require('fs');
+  const xlsx = require('xlsx');
+  const workbook = xlsx.readFile('.\\Tabla de valores.xlsx');
+  const sheet_name_list = workbook.SheetNames;
+  const data = xlsx.utils.sheet_to_json(workbook.Sheets[sheet_name_list[0]]);
   
-  // Tarifas para diferentes modelos de automóvil
-  switch (modelo) {
-  case "corsa":
-  costo = 500;
-  break;
-  case "fiesta":
-  costo = 700;
-  break;
-  case "focus":
-  costo = 1000;
-  break;
-  default:
-  costo = 400;
+  const vehiculos = [];
+  
+  const columnNames = Object.keys(data[0]);
+  
+  for (let i = 0; i < data.length; i++) {
+    const object = {};
+    for (let j = 0; j < columnNames.length; j++) {
+      object[columnNames[j]] = data[i][columnNames[j]];
+    }
+    vehiculos.push(object);
   }
-  
-  // Tarifas adicionales para diferentes edades de conductor
-  if (edad < 25) {
-  costo += 200;
-  } else if (edad >= 25 && edad < 35) {
-  costo += 150;
-  }
-  
-  // Tarifas adicionales para diferentes lugares de residencia
-  switch (lugar) {
-  case "urbana":
-  costo += 200;
-  break;
-  case "rural":
-  costo -= 100;
-  break;
-  default:
-  break;
-  }
-  
-  // Devolver el costo total del seguro
-  return costo;
-  }
-  
-  // Variables para almacenar la información del usuario
-  let modelo;
-  let edad;
-  let lugar;
-  let costoSeguro;
-  
-  // Solicitar y validar el modelo del automóvil
-  do {
-  modelo = prompt("Ingresa el modelo de tu automóvil (corsa, fiesta, focus):");
-  if (!["corsa", "fiesta", "focus"].includes(modelo)) {
-  alert("El modelo no se encuentra en el sistema (probá con corsa, fiesta, focus).");
-  }
-  } while (!["corsa", "fiesta", "focus"].includes(modelo));
-  
-  // Solicitar y validar la edad del conductor
-  do {
-  edad = parseInt(prompt("Ingresa tu edad:"));
-  if (isNaN(edad) || edad < 18) {
-  alert("Por favor, ingresa una edad válida (mayor o igual a 18 años).");
-  }
-  } while (isNaN(edad) || edad < 18);
-  
-  // Solicitar y validar el lugar de residencia
-  do {
-  lugar = prompt("Ingresa tu zona (urbana, rural):");
-  if (!["urbana", "rural"].includes(lugar)) {
-  alert("Por favor, ingresa un lugar válido (urbana, rural).");
-  }
-  } while (!["urbana", "rural"].includes(lugar));
-  
-  // Calcular el costo del seguro de automóvil usando la función
-  costoSeguro = calcularSeguroAuto(modelo, edad, lugar);
-  
-  // Mostrar el resultado al usuario
-  alert(`El costo de tu seguro de automóvil es de $${costoSeguro}`);
+
+  const vehiculosPorMarca = vehiculos.filter((el) => el.MARCA === marca);
+  const vehiculosPorModelo = vehiculosPorMarca.filter((el) => el.MODELO === modelo);
+  const vehiculosPorVersion = vehiculosPorModelo.filter((el) => el.VERSION === version);
+  const vehiculosPorAno = vehiculosPorVersion.filter((el) => el.ANO === ano);
+
+  let sumaAsegurada = vehiculosPorAno[0].SA
+
+  return sumaAsegurada
+}
+
+console.log(`La suma asegurada es ${consultarSA("AGRALE", "AGRALE", "10000  S", 2016)}`)
+
+// let marca = prompt("Ingrese la marca:")
+// let modelo = prompt("Ingrese el modelo: ")
+// let version = prompt("Ingrese la versión: ")
+// let ano = parseInt(prompt("Ingrese el año: "))
+
+// let sa = consultarSA(marca, modelo, version, ano)
+
+// alert(`La suma asegurada es ${sa}`)
+
