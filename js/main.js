9771dc6888b2beb0a23103e7649c3d5146f33ae4
Inti-Kuniss <126829303+Inti-Kuniss@users.noreply.github.com>
2023-05-30 21:33:45 -0300

    Se agregan eventos, DOM y Storage

--- js/main.js
+++ js/main.js
@@ -1,40 +1,112 @@
-function consultarSA(marca, modelo, version, ano){
-  const fs = require('fs');
-  const xlsx = require('xlsx');
-  const workbook = xlsx.readFile('.\\Tabla de valores.xlsx');
-  const sheet_name_list = workbook.SheetNames;
-  const data = xlsx.utils.sheet_to_json(workbook.Sheets[sheet_name_list[0]]);
-  
-  const vehiculos = [];
-  
-  const columnNames = Object.keys(data[0]);
-  
-  for (let i = 0; i < data.length; i++) {
-    const object = {};
-    for (let j = 0; j < columnNames.length; j++) {
-      object[columnNames[j]] = data[i][columnNames[j]];
+// Función asincrónica para consultar la suma asegurada
+async function consultarSA(marca, modelo, version, ano) {
+  try {
+    // Obtener el archivo JSON de vehículos
+    const response = await fetch('./vehiculos.json');
+
+    // Verificar si la respuesta es exitosa
+    if (!response.ok) {
+      throw new Error('Error al cargar el archivo JSON.');
     }
-    vehiculos.push(object);
+
+    // Obtener el JSON como un array
+    const jsonArray = await response.json();
+
+    // Filtrar los vehículos por marca, modelo, versión y año
+    const vehiculosPorAno = jsonArray.filter(el =>
+      el.MARCA === marca &&
+      el.MODELO === modelo &&
+      el.VERSION === version &&
+      el.ANO === ano
+    );
+
+    // Obtener la suma asegurada del primer vehículo coincidente (si existe)
+    const sumaAsegurada = vehiculosPorAno[0]?.SA;
+
+    return sumaAsegurada;
+
+  } catch (error) {
+    console.log(error);
+    throw error;
   }
-
-  const vehiculosPorMarca = vehiculos.filter((el) => el.MARCA === marca);
-  const vehiculosPorModelo = vehiculosPorMarca.filter((el) => el.MODELO === modelo);
-  const vehiculosPorVersion = vehiculosPorModelo.filter((el) => el.VERSION === version);
-  const vehiculosPorAno = vehiculosPorVersion.filter((el) => el.ANO === ano);
-
-  let sumaAsegurada = vehiculosPorAno[0].SA
-
-  return sumaAsegurada
 }
 
-console.log(`La suma asegurada es ${consultarSA("AGRALE", "AGRALE", "10000  S", 2016)}`)
+// Obtener el formulario y establecer variables de seguimiento
+const formulario = document.getElementById("formulario");
+let divPrevio = null;
+let divCotizacionesHistoricasPrevio = null;
 
-// let marca = prompt("Ingrese la marca:")
-// let modelo = prompt("Ingrese el modelo: ")
-// let version = prompt("Ingrese la versión: ")
-// let ano = parseInt(prompt("Ingrese el año: "))
+// Manejador de eventos para el envío del formulario
+formulario.addEventListener("submit", async (e) => {
+  e.preventDefault();
 
-// let sa = consultarSA(marca, modelo, version, ano)
+  // Eliminar la cotización anterior (si existe)
+  if (divPrevio) {
+    divPrevio.remove();
+  }
 
-// alert(`La suma asegurada es ${sa}`)
+  // Eliminar las cotizaciones históricas anteriores (si existen)
+  if (divCotizacionesHistoricasPrevio) {
+    divCotizacionesHistoricasPrevio.remove();
+  }
 
+  // Obtener los valores del formulario
+  const { nombre, apellido, marca, modelo, version, ano } = e.target.elements;
+  const nombreCompleto = `${nombre.value} ${apellido.value}`;
+
+  // Consultar la suma asegurada
+  const sa = await consultarSA(marca.value, modelo.value, version.value, parseInt(ano.value));
+
+  // Obtener las cotizaciones históricas almacenadas en sessionStorage o un array vacío
+  const cotizacionesHistoricas = JSON.parse(sessionStorage.getItem("cotizacionesHistoricas")) || [];
+
+  // Crear el objeto de la cotización actual
+  const cotizacionActual = {
+    nombreCompleto,
+    marca: marca.value,
+    modelo: modelo.value,
+    version: version.value,
+    ano: parseInt(ano.value),
+    sumaAsegurada: sa
+  };
+
+  // Mostrar la cotización actual en el DOM
+  const divCotizacionActual = document.createElement("div");
+  divCotizacionActual.innerHTML = `
+    <h1>Tus datos:</h1>
+    <p>Nombre completo: ${nombreCompleto}</p>
+    <p>Marca: ${marca.value}</p>
+    <p>Modelo: ${modelo.value}</p>
+    <p>Versión: ${version.value}</p>
+    <p>Año: ${ano.value}</p>
+    <p>Suma asegurada: $${sa?.toLocaleString()}</p>
+  `;
+  document.body.append(divCotizacionActual);
+  divPrevio = divCotizacionActual;
+
+  // Agregar la cotización actual al array de cotizaciones históricas
+  cotizacionesHistoricas.push(cotizacionActual);
+  sessionStorage.setItem("cotizacionesHistoricas", JSON.stringify(cotizacionesHistoricas));
+
+  // Mostrar las cotizaciones históricas en el DOM
+  const divCotizacionesHistoricas = document.createElement("div");
+  divCotizacionesHistoricas.innerHTML = "<h2>Cotizaciones Históricas:</h2>";
+
+  // Recorrer las cotizaciones históricas (excluyendo la cotización actual) y mostrarlas en el DOM
+  cotizacionesHistoricas.slice(0, -1).forEach((cotizacion, index) => {
+    const divCotizacion = document.createElement("div");
+    divCotizacion.innerHTML = `
+      <h3>Cotización ${index + 1}:</h3>
+      <p>Nombre completo: ${cotizacion.nombreCompleto}</p>
+      <p>Marca: ${cotizacion.marca}</p>
+      <p>Modelo: ${cotizacion.modelo}</p>
+      <p>Versión: ${cotizacion.version}</p>
+      <p>Año: ${cotizacion.ano}</p>
+      <p>Suma asegurada: $${cotizacion.sumaAsegurada?.toLocaleString()}</p>
+    `;
+    divCotizacionesHistoricas.append(divCotizacion);
+  });
+
+  document.body.append(divCotizacionesHistoricas);
+  divCotizacionesHistoricasPrevio = divCotizacionesHistoricas;
+});
